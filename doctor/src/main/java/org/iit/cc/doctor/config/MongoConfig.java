dca6e4e9fa87df509c72524c55c3a9b11338766a
Dhananjana Milinda <dhananjana.20240366@iit.ac.lk>
2024-12-17 13:05:13 +0530

    [IIT] applied changes related mongo db configurations to few services

---
+++ doctor/src/main/java/org/iit/cc/doctor/config/MongoConfig.java
@@ -0,0 +1,15 @@
+package org.iit.cc.doctor.config;
+
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(MongoClients.create("mongodb+srv://dhananjana20240366:nGfImOQdee3lXdY7@cloudcomputing.zkv5y.mongodb.net/?retryWrites=true&w=majority&appName=CloudComputing"), "Doctor");
+    }
+}
---
+++ notification-service/src/main/java/org/iit/cc/notificationservice/config/MongoConfig.java
@@ -0,0 +1,16 @@
+package org.iit.cc.notificationservice.config;
+
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(MongoClients.create("mongodb+srv://dhananjana20240366:nGfImOQdee3lXdY7@cloudcomputing.zkv5y.mongodb.net/?retryWrites=true&w=majority&appName=CloudComputing"), "Notification");
+    }
+}
+
